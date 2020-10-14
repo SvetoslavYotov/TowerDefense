e0e958d2c20f17e5a561cdda0dd2c31255784120
Svetoslav <syotov89@gmail.com>
2020-10-14 22:36:43 +0300

    01. Cube from quads + snapping script

---
+++ Assets/EditorSnap.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class EditorSnap : MonoBehaviour
+{
+    Vector3 snapPos;
+    [SerializeField][Range(1f, 20f)] float gridSize = 10f;
+
+    void Update()
+    {
+        snapPos.x = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize;
+        snapPos.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
+
+        transform.position = new Vector3(snapPos.x, 0f, snapPos.z);
+    }
+
+}
