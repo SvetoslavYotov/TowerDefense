5c72352108080fe9cb66a86de83613efdfe3e09d
Svetoslav <syotov89@gmail.com>
2020-10-19 21:19:24 +0300

    04. Coroutines.

--- Assets/EnemyMovement.cs
+++ Assets/EnemyMovement.cs
@@ -1,15 +1,30 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
 
-    [SerializeField] List<Block> path;
+    [SerializeField] List<Waypoint> path;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(FollowPath());
+    }
+
+    IEnumerator FollowPath()
+    {
+        print("Starting Patrol!");
+        foreach (var waypoint in path)
+        {
+            transform.position = waypoint.transform.position;
+            print("Visiting block:" + waypoint.name);
+            yield return new WaitForSeconds(1f);
+        }
+        print("Ending patrol");
     }
 
     // Update is called once per frame
