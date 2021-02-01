b4021d240baa218a8f73ba65a444031775a7d0f5
Svetoslav <syotov89@gmail.com>
2021-02-01 13:47:06 +0200

    09. Destroying enemy

---
+++ Assets/EnemyDamage.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDamage : MonoBehaviour
+{
+    [SerializeField] int hitPoint = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnParticleCollision(GameObject other)
+    {
+        
+        ProcessHit();
+        if (hitPoint < 1)
+        {
+            KillEnemy();
+        }
+    }
+
+    void ProcessHit()
+    {
+        hitPoint -= 1;
+        print("Current hit points are: " + hitPoint);
+    }
+
+    private void KillEnemy()
+    {
+        Destroy(gameObject);
+    }
+}
