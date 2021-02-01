af4669d30adb67edd78d03d2c52db0d29733162b
Svetoslav <syotov89@gmail.com>
2021-02-01 14:26:26 +0200

    10. Setting range/Toggle On/Off fire

--- Assets/Tower.cs
+++ Assets/Tower.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,9 +8,33 @@ public class Tower : MonoBehaviour
 
     [SerializeField] Transform objectToPan;
     [SerializeField] Transform targetedEnemy;
+    [SerializeField] float attackRange = 10.0f;
+    [SerializeField] ParticleSystem projectilePartile;
+
+
     // Update is called once per frame
     void Update()
     {
-        objectToPan.LookAt(targetedEnemy);
+        if (targetedEnemy)
+        {
+            objectToPan.LookAt(targetedEnemy);
+            FireAtEnemy();
+        }
+        else
+        {
+            Shoot(false);
+        }
+    }
+
+    private void FireAtEnemy()
+    {
+        var distanceToEnemy = Vector3.Distance(targetedEnemy.transform.position, gameObject.transform.position);
+        Shoot(distanceToEnemy <= attackRange ? true : false);
+    }
+
+    private void Shoot(bool isActive)
+    {
+        var emissionModule = projectilePartile.emission;
+        emissionModule.enabled = isActive;
     }
 }
